530f7a0c3030c3448b205f806237c8f6f41b1aef
JimmyCJM <chijieming@sina.com>
2017-07-24 23:00:27 +0800

    Update practice-2.js

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,44 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var result = new Array();
+  var count = 1;
+  var a=collection.pop();
+  for(var i=0;i<collection.length;i++){
+    if (collection[i]==collection[i+1]){
+      count++;
+    }
+    else{
+      result.push({key:collection[i],count:count});
+      count=1;
+    }
+  }
+  result.push({key:a.split("-")[0],count:parseInt(a.split("-")[1])});
+  return result;
 }
+
+
+/*	第一次代码
+function countSameElements(collection) {
+  var result = new Array();
+  var count = 1;
+  var n = 0;
+  var a=collection.pop();
+  function Object(key,count){
+    this.key=key;
+    this.count=count;
+  }
+  for(var i=0;i<collection.length;i++){
+    if (collection[i]==collection[i+1]){
+      count++;
+    }
+    else{
+      result[n]=new Object(collection[i],count);
+      count=1;
+      n++;
+    }
+  }
+  result[n]=new Object(a.split("-")[0],parseInt(a.split("-")[1])});
+  return result;
+}
+*/
