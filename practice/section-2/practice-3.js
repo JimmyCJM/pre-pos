66d6c9328387cf49ea1652a3213a58a415acd9f7
JimmyCJM <chijieming@sina.com>
2017-07-24 23:06:05 +0800

    Update practice-3.js

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -7,7 +7,7 @@ function countSameElements(collection) {
   var sameCount = new Array();
   var sameCollection = new Array();
   var newCollection = new Array();
-  //预处理数组，将其他表示方式修改
+  //预处理数组，将其他表示方式表示个数的方式删除，修改collection为newCollection
   for (var i=0;i<collection.length;i++) {
     newCollection[i] = collection[i];
     if (collection[i].length != 1) {
@@ -17,6 +17,7 @@ function countSameElements(collection) {
       n++;
     }
   }
+  //将samCollection和sameCount中的重复项合并
   for(var i=0;i<n;i++){
     if (sameCollection[i] == sameCollection[i+1]) {
       sameCollection.splice(i,1);
@@ -24,20 +25,21 @@ function countSameElements(collection) {
       sameCount[i] += a;
     }
   }
-
+  //计算个数
   var k = n;
   for (var i=0;i<newCollection.length;i++){
-    if (newCollection[i] == newCollection[i+1]){
+    if (newCollection[i] == newCollection[i+1]){  
       summary++;
     }
     else{
-      if(newCollection[i] == sameCollection[n-k]){
+      if(newCollection[i] == sameCollection[n-k]){  
         summary += sameCount[n-k];
         k--;
-      }
+      } //将之前删除的个数补上
       result.push({name:newCollection[i],summary:summary});
       summary = 1;
     }
   }
   return result;
 }
+
