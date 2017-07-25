c325b09ec27dc96e0236b10fc5f4e26f58486779
JimmyCJM <chijieming@sina.com>
2017-07-25 10:28:25 +0800

    Update practice-2.js

--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -1,5 +1,16 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var result = collectionA;
+  for (var i=0;i<collectionA.length;i++){
+    for (var j=0;j<objectB.value.length;j++){
+      if (collectionA[i].key == objectB.value[j]){
+        if (collectionA[i].count/3 > 0){
+          var a = collectionA[i].count - parseInt(collectionA[i].count/3);
+        }
+        result[i] = ({key:collectionA[i].key,count:a})；
+      }
+    }
+  }
+  return result;
 }
